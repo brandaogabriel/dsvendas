08e0b1624c933b0de30f2750c4b846cf2f5816a8
Gabriel Brandao <gabriel.brandao@ccc.ufcg.edu.br>
2021-06-22 00:30:01 -0300

    feat: add domain model, database seed

---
+++ backend/src/main/java/com/devgabriel/dsvendas/entities/Seller.java
@@ -0,0 +1,63 @@
+package com.devgabriel.dsvendas.entities;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_seller")
+public class Seller implements Serializable {
+  private static final long serialVersionUID = 1L;
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+  private Long id;
+  private String name;
+
+  @OneToMany(mappedBy = "seller")
+  private final List<Sale> sales = new ArrayList<>();
+
+  public Seller() {
+  }
+
+  public Seller(Long id, String name) {
+    this.id = id;
+    this.name = name;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public List<Sale> getSales() {
+    return sales;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    Seller seller = (Seller) o;
+    return Objects.equals(id, seller.id);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(id);
+  }
+}
